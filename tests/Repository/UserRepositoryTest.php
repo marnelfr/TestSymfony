117ce9207d1e2960da1bbe2d238f54361f7174e4
Marnel Fresh'eur <gmlginolias@gmail.com>
2020-11-21 09:27:59 +0100

    Loading fixtures with LiipTestFixturesBundle. But it doesn't work as expected

--- tests/Repository/UserRepositoryTest.php
+++ tests/Repository/UserRepositoryTest.php
@@ -8,15 +8,19 @@
 
 namespace App\Tests\Repository;
 
+use App\DataFixtures\UserFixtures;
 use App\Repository\UserRepository;
+use Liip\TestFixturesBundle\Test\FixturesTrait;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class UserRepositoryTest extends KernelTestCase
 {
+    use FixturesTrait;
 
     public function testCount(): void
     {
         self::bootKernel();
+        $this->loadFixtures([UserFixtures::class]);
         $repositoy = self::$container->get(UserRepository::class); //utiliser le container par defaut ne permet pas de recuperer les services privés
         $totalUser = $repositoy->count([]);
         $this->assertEquals(10, $totalUser);
